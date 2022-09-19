6d37b17a9a12cc95de52ba7af784ed74091a958e
Harsha R <99862806+harshaR2727@users.noreply.github.com>
2022-09-19 11:07:09 +0530

    Add files via upload

---
+++ This_key_word.java
@@ -0,0 +1,19 @@
+package com.tns.assingments;
+
+public class This_key_word {
+
+	int rollno;
+	public  This_key_word(int rollno){
+		this.rollno=rollno;
+	}
+
+
+
+
+	public static void main(String[] args) {
+
+		This_key_word rollno=new This_key_word(5);
+		System.out.println("the rollno is ="+rollno.rollno);
+	}
+
+}
